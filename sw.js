387ff760047eba82ed9e79f24826c565de1055a6
Taras Yurchyshyn <yts.prof@gmail.com>
2026-02-20 14:18:16 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'pwa-v1';
+const ASSETS = [
+    '/',
+    '/index.html'
+];
+
+// Встановлення
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Активація
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+// Перехоплення запитів
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            return cached || fetch(event.request).then((response) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        }).catch(() => caches.match('/'))
+    );
+});
